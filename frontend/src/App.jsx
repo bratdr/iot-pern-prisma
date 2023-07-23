b0c3b9e913979e02d6d796ac6b49f844edf1b0ca
Brata Darmawan <bratdr@iMac-Pro.local>
2023-07-23 15:53:32 +0700

    update App.jsx route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,9 +14,11 @@ import EditDriver from "./pages/LoginAdmin/ManageDriver/Edit";
 import ManageSchool from "./pages/LoginAdmin/ManageSchool";
 import AddSchool from "./pages/LoginAdmin/ManageSchool/Add";
 import EditSchool from "./pages/LoginAdmin/ManageSchool/Edit";
+import SiswaSchool from "./pages/LoginAdmin/ManageSchool/Siswa";
 import ManageBuses from "./pages/LoginAdmin/ManageBuses";
 import AddBuses from "./pages/LoginAdmin/ManageBuses/Add";
 import EditBuses from "./pages/LoginAdmin/ManageBuses/Edit";
+import ConnectSiswa from "./pages/LoginAdmin/ManageSchool/ConnectSiswa";
 
 function App() {
   return (
@@ -26,7 +28,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login/admin" element={<LoginAdmin />} />
           <Route path="/login/user" element={<LoginUser />} />
-
           <Route path="/user/dashboard" element={<Dashboard />} />
           <Route path="/user/dashboard/history" element={<History />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
@@ -39,6 +40,15 @@ function App() {
           <Route path="/admin/dashboard/school" element={<ManageSchool />} />
           <Route path="/admin/dashboard/school/add" element={<AddSchool />} />
           <Route path="/admin/dashboard/school/edit" element={<EditSchool />} />
+          <Route
+            path="/admin/dashboard/school/:schoolId/siswa"
+            element={<SiswaSchool />}
+          />{" "}
+          {/* Add the SiswaList route with the correct location */}
+          <Route
+            path="/admin/dashboard/school/connect/siswa"
+            element={<ConnectSiswa />}
+          />
           <Route path="/admin/dashboard/buses/" element={<ManageBuses />} />
           <Route path="/admin/dashboard/buses/add" element={<AddBuses />} />
           <Route path="/admin/dashboard/buses/edit" element={<EditBuses />} />
