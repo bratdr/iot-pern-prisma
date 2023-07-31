c2a2336ad7a6ea9a41aed9e949d40c7c51907195
bratdr <bratadarmawan@gmail.com>
2023-07-31 09:45:13 +0700

    update Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import LoginUser from "./pages/LoginUser";
 import Dashboard from "./pages/LoginUser/Dashboard";
 import AdminDashboard from "./pages/LoginAdmin/Dashboard";
 import History from "./pages/LoginUser/History";
-import ManageUser from "./pages/LoginAdmin/ManageUser";
-import AddUser from "./pages/LoginAdmin/ManageUser/Add";
-import EditUser from "./pages/LoginAdmin/ManageUser/Edit";
+import ManageSiswa from "./pages/LoginAdmin/ManageSiswa";
+import AddSiswa from "./pages/LoginAdmin/ManageSiswa/Add";
+import EditSiswa from "./pages/LoginAdmin/ManageSiswa/Edit";
 import ManageDriver from "./pages/LoginAdmin/ManageDriver";
 import AddDriver from "./pages/LoginAdmin/ManageDriver/Add";
 import EditDriver from "./pages/LoginAdmin/ManageDriver/Edit";
@@ -19,6 +19,8 @@ import ManageBuses from "./pages/LoginAdmin/ManageBuses";
 import AddBuses from "./pages/LoginAdmin/ManageBuses/Add";
 import EditBuses from "./pages/LoginAdmin/ManageBuses/Edit";
 import ConnectSiswa from "./pages/LoginAdmin/ManageSchool/ConnectSiswa";
+import SetSupir from "./pages/LoginAdmin/ManageBuses/Set";
+import TestMap from "./pages/TestMap";
 
 function App() {
   return (
@@ -31,9 +33,9 @@ function App() {
           <Route path="/user/dashboard" element={<Dashboard />} />
           <Route path="/user/dashboard/history" element={<History />} />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/dashboard/user" element={<ManageUser />} />
-          <Route path="/admin/dashboard/user/add" element={<AddUser />} />
-          <Route path="/admin/dashboard/user/edit" element={<EditUser />} />
+          <Route path="/admin/dashboard/siswa" element={<ManageSiswa />} />
+          <Route path="/admin/dashboard/siswa/add" element={<AddSiswa />} />
+          <Route path="/admin/dashboard/siswa/edit" element={<EditSiswa />} />
           <Route path="/admin/dashboard/driver" element={<ManageDriver />} />
           <Route path="/admin/dashboard/driver/add" element={<AddDriver />} />
           <Route path="/admin/dashboard/driver/edit" element={<EditDriver />} />
@@ -52,6 +54,8 @@ function App() {
           <Route path="/admin/dashboard/buses/" element={<ManageBuses />} />
           <Route path="/admin/dashboard/buses/add" element={<AddBuses />} />
           <Route path="/admin/dashboard/buses/edit" element={<EditBuses />} />
+          <Route path="/admin/dashboard/buses/set" element={<SetSupir />} />
+          <Route path="/map/test" element={<TestMap />} />
         </Routes>
       </Router>
     </>
